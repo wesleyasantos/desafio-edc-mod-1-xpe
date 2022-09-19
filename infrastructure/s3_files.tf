7f2e2ed2964226073c2f232d200c3209018b188d
Wesley Andrade <wesleydevds@gmail.com>
2022-09-19 13:04:48 -0300

    corrige codigo da lambda_function e spark_processing

--- infrastructure/s3_files.tf
+++ infrastructure/s3_files.tf
@@ -6,26 +6,10 @@ resource "aws_s3_bucket_object" "lambda_function" {
   etag   = filemd5("../etl/lambda_function.py")
 }
 
-resource "aws_s3_bucket_object" "spark_transform" {
-  bucket = aws_s3_bucket.datalake.id
-  key    = "pyspark/spark_transform.py"
-  acl    = "private"
-  source = "../etl/spark_transform.py"
-  etag   = filemd5("../etl/spark_transform.py")
-}
-
 resource "aws_s3_bucket_object" "spark_processing" {
   bucket = aws_s3_bucket.datalake.id
   key    = "pyspark/spark_processing.py"
   acl    = "private"
   source = "../etl/spark_processing.py"
   etag   = filemd5("../etl/spark_processing.py")
-}
-
-resource "aws_s3_bucket_object" "spark_processed" {
-  bucket = aws_s3_bucket.datalake.id
-  key    = "pyspark/spark_processed.py"
-  acl    = "private"
-  source = "../etl/spark_processed.py"
-  etag   = filemd5("../etl/spark_processed.py")
 }
